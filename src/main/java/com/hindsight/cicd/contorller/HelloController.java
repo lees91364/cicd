a3fafc98817e4f37925cdd53359c070e06a03144
이규준 <lees91364@gmail.com>
2021-08-06 14:44:42 +0900

    ff

--- src/main/java/com/hindsight/cicd/contorller/HelloController.java
+++ src/main/java/com/hindsight/cicd/contorller/HelloController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
+
+
     public String hello(){
         return "Hello #2";
     }
